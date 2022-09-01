6e21fa2b9cf2446e9bcf0236ad8b8a0dda5a7d0c
David Wright <davecwright@knights.ucf.edu>
2022-08-31 20:55:56 -0400

    Add IGW model file

---
+++ inputs/models/igw.py
@@ -0,0 +1,65 @@
+import enterprise.signals.parameter as parameter
+import src.models_utils as aux
+import numpy as np
+import scipy.constants as scon
+import natpy as nat
+
+name = 'igw' # name of the model
+
+smbhb = False # set to True if you want to overlay the new-physics signal to the SMBHB signal
+
+parameters = {
+    'n_t':parameter.Uniform(-2,2)('n_t'),
+    'log10_r':parameter.Uniform(-7,0)('log10_r'),
+    'log10_T_rh':parameter.Uniform(-6,16)('log10_T_rh') # this in GeV
+    }
+
+group = []
+
+
+def transfer_func(f: float, f_rh: float) -> float:
+    """Calculate the transfer function as a function of GW frequency."""
+
+
+    f_ratio = f / f_rh
+
+    return (1 - 0.22 * f_ratio**1.5 + 0.65**f_ratio**2)**-1
+
+def power_spec(f: float, n_t: float, r: float) -> float:
+    """Calculate primordial tensor power spectrum."""
+
+    return r * aux.A_s * ( f / aux.f_cmb )**n_t
+
+
+@aux.omega2cross
+def spectrum(f: float, n_t: float, log10_r: float, log10_T_rh: float) -> float:
+    """Calculate GW energy density.
+
+    Returns the GW energy density as a fraction of the closure density as a
+    function of the parameters of the model:
+
+    :param float f: Frequency in Hz.
+    :param float n_t: Tensor spectral index.
+    :param float log10_r: Tensor to scalar ratio in log10 space.
+    :param float log10_T_rh: Temperature [GeV] at reheating in log10 space.
+    :returns: GW energy density as a fraction of the closure density.
+    :rtype: float
+    """
+
+    r = 10**log10_r
+    T_rh = 10**log10_T_rh
+    f_rh = aux.freq_at_temp(T_rh)
+
+    if f <= f_rh:
+        prefactor = (
+            ( aux.omega_r / 24 ) * ( aux.g_rho(aux.temp_at_freq(f))[0] / aux.g_rho_0 ) *
+            ( aux.g_s_0 / aux.g_s(aux.temp_at_freq(f))[0] )**4/3
+            )
+
+    else:
+         prefactor = (
+            ( aux.omega_r / 24 ) * ( aux.g_rho(aux.temp_at_freq(f_rh))[0] / aux.g_rho_0 ) *
+            ( aux.g_s_0 / aux.g_s(aux.temp_at_freq(f_rh))[0] )**4/3
+            )
+
+    return aux.h**2 * prefactor * power_spec(f, n_t, r) * transfer_func(f, f_rh)
