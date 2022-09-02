ae8c230ad2f7718278d0118443671e05c5b5cab9
David Wright <davecwright@knights.ucf.edu>
2022-09-02 13:18:41 -0400

    Add docs to igw.py

--- inputs/models/igw.py
+++ inputs/models/igw.py
@@ -1,13 +1,12 @@
 import enterprise.signals.parameter as parameter
 import src.models_utils as aux
 import numpy as np
-import scipy.constants as scon
-import natpy as nat
-import numpy.typing as npt
+from numpy.typing import NDArray
+from typing import Union
 
 name = 'igw' # name of the model
 
-smbhb = False # set to True if you want to overlay the new-physics signal to the SMBHB signal
+smbhb = True # set to True if you want to overlay the new-physics signal to the SMBHB signal
 
 parameters = {
     'n_t':parameter.Uniform(-2,2)('n_t'),
@@ -18,33 +17,45 @@ parameters = {
 group = []
 
 
-def transfer_func(f: float, f_rh: float) -> float:
-    """Calculate the transfer function as a function of GW frequency."""
+def transfer_func(f: Union[NDArray,float], f_rh: Union[NDArray,float]) -> Union[NDArray,float]:
+    """Calculate the transfer function as a function of GW frequency.
+
+    :param Union[NDArray, float] f: Frequency of GW in Hz
+    :param Union[NDArray, float] f_rh: Frequency of GW emitted at temp of reheating T_rh
+    :return: Transfer function of a GW with frequency f
+    :rtype: Union[NDArray, float]
+    """
 
 
     f_ratio = f / f_rh
 
     return (1 - 0.22 * f_ratio**1.5 + 0.65**f_ratio**2)**-1
 
-def power_spec(f: float, n_t: float, r: float) -> float:
-    """Calculate primordial tensor power spectrum."""
+def power_spec(f: Union[NDArray,float], n_t: float, r: float) -> Union[NDArray,float]:
+    """Calculate primordial tensor power spectrum.
+
+    :param Union[NDArray, float] f: Frequency [Hz] of GW
+    :param float n_t: Tensor spectral index
+    :param float r: Tensor to scalar ratio
+    :return: Primordial tensor power spectrum
+    :rtype: Union[NDArray, float]"""
 
     return r * aux.A_s * ( f / aux.f_cmb )**n_t
 
 
 @aux.omega2cross
-def spectrum(f: npt.NDArray, n_t: float, log10_r: float, log10_T_rh: float) -> npt.NDArray:
+def spectrum(f: Union[NDArray,float], n_t: float, log10_r: float, log10_T_rh: float) -> Union[NDArray,float]:
     """Calculate GW energy density.
 
     Returns the GW energy density as a fraction of the closure density as a
     function of the parameters of the model:
 
-    :param float f: Frequency in Hz.
-    :param float n_t: Tensor spectral index.
-    :param float log10_r: Tensor to scalar ratio in log10 space.
-    :param float log10_T_rh: Temperature [GeV] at reheating in log10 space.
-    :returns: GW energy density as a fraction of the closure density.
-    :rtype: float
+    :param Union[NDArray, float] f: Frequency of GW in Hz
+    :param float n_t: Tensor spectral index
+    :param float log10_r: Tensor to scalar ratio in log10 space
+    :param float log10_T_rh: Temperature [GeV] at reheating in log10 space
+    :return: GW energy density as a fraction of the closure density
+    :rtype: Union[NDArray, float]
     """
 
     r = 10**log10_r
