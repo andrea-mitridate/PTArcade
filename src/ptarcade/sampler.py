0f730ad278ba45c2cbd7524bf875fe6dd17e0cdc
David Wright <davecwright@knights.ucf.edu>
2023-06-14 01:28:00 -0400

    Split up sampler into multiple functions

--- src/ptarcade/sampler.py
+++ src/ptarcade/sampler.py
@@ -1,97 +1,124 @@
-import os
-import sys
+"""Module for sampling using [PTMCMCSampler][] and [enterprise_extensions][]."""
+from __future__ import annotations
 
+import os
 import warnings
+
 from astropy.utils.exceptions import AstropyDeprecationWarning
+
 warnings.filterwarnings('ignore', category=AstropyDeprecationWarning)
 
-from enterprise_extensions import hypermodel
-
-import src.pta_importer as pta_importer
-import src.signal_builder as signal_builder
-import src.input_handler as input_handler
-
-import time
 import platform
+import time
+from types import ModuleType
+from typing import Any
 
-try:
-    import cpuinfo
-    def cpu_model():
+from enterprise.pulsar import Pulsar
+from enterprise.signals.signal_base import PTA
+from enterprise_extensions import hypermodel
+from ptarcade.input_handler import bcolors
+from numpy._typing import _ArrayLikeFloat_co as array_like
+from numpy.typing import NDArray
+from PTMCMCSampler.PTMCMCSampler import PTSampler
+
+from ptarcade import input_handler, pta_importer, signal_builder
+
+
+def cpu_model() -> str:
+    """Get CPU info."""
+    try:
+        import cpuinfo
         return cpuinfo.get_cpu_info()["brand_raw"]
-
-except ModuleNotFoundError:
-    def cpu_model():
+    except ModuleNotFoundError:
         return "unknown CPU (for better info install py-cpuinfo)"
 
-print('\n--- Starting to run ---\n')
-print("\tNode", platform.node(), cpu_model(),"\n");
 
-start_cpu = time.process_time()
-start_real = time.perf_counter()
+def get_user_args() -> tuple[dict[str, ModuleType], dict[str, Any]] :
+    """Get CLI arguments
 
-###############################################################################
-# load inputs
-###############################################################################
+    Returns
+    -------
+    inputs : dict[str, ModuleType]
+        Dictionary of loaded user-supplied modules.
+    input_options : dict[str, Any]
+        Dictionary of user-supplied input options.
 
-# parse command line inputs
-input_options, cmd_input_okay = input_handler.get_cmdline_arguments()
+    Raises
+    ------
+    SystemExit
+        If CLI input is missing required args.
 
-if not cmd_input_okay:
+    """
+    # parse command line inputs
+    input_options, cmd_input_okay = input_handler.get_cmdline_arguments()
 
-    print('ERROR:')
-    print("\t- Model file must be provided via the -m input flag.\n"+
-    "\t Add -h (--help) flags for more help.")
+    if not cmd_input_okay:
 
-    sys.exit()
+        error = (f"{bcolors.FAIL}ERROR{bcolors.ENDC}: Model and configuration files must be present\n"
+        "\t- These are added with the -m, -c input flags. Add -h (--help) flags for more help.\n")
 
-inputs = input_handler.load_inputs(input_options)
+        raise SystemExit(error)
 
-inputs['config'] = input_handler.check_config(inputs['config'])
+    inputs = input_handler.load_inputs(input_options)
+    inputs['config'] = input_handler.check_config(inputs['config'])
 
-###############################################################################
-# load pulsars and noise parameters
-###############################################################################
-print('--- Loading Pulsars and noise data ... ---\n')
+    return inputs, input_options
 
-# import pta data
-psrs, noise_params, emp_dist = pta_importer.pta_data_importer(inputs['config'].pta_data)
 
-print(f"\tloaded {len(psrs)} pulsars\n")
+def get_user_pta_data(inputs: dict[str, Any]) -> tuple[list[Pulsar], dict | None, array_like | None ]:
+    """Import user-specified PTA data.
 
-input_handler.check_model(
-    model=inputs['model'],
-    psrs=psrs,
-    red_components=inputs['config'].red_components,
-    gwb_components=inputs['config'].gwb_components)
+    Parameters
+    ----------
+    inputs : dict[str, Any]
+        User supplied modules
 
-print('--- Done loading Pulsars and noise data. ---\n')
+    Returns
+    -------
+    psrs : list[Pulsar]
+        List of Pulsar objects
+    noise_params : dict | None
+        Dictionary containing noise data
+    emp_dist : array_like | None
+        The empirical distribution to use for sampling
 
-###############################################################################
-# define models and initialize PTA
-###############################################################################
-print('--- Initializing PTA ... ---\n')
+    """
+    # import pta data
+    psrs, noise_params, emp_dist = pta_importer.pta_data_importer(inputs['config'].pta_data)
 
-pta = {}
+    print(f"\tloaded {len(psrs)} pulsars\n")
 
-pta[0] = signal_builder.builder(
-    psrs=psrs, 
-    model=inputs['model'], 
-    noisedict=noise_params,
-    pta_dataset=inputs['config'].pta_data,
-    bhb_th_prior=inputs['config'].bhb_th_prior,
-    gamma_bhb=inputs['config'].gamma_bhb,
-    A_bhb_logmin=inputs['config'].A_bhb_logmin,
-    A_bhb_logmax=inputs['config'].A_bhb_logmax,
-    corr=inputs['config'].corr,
-    red_components=inputs["config"].red_components, 
-    gwb_components=inputs["config"].gwb_components)
+    input_handler.check_model(
+        model=inputs['model'],
+        psrs=psrs,
+        red_components=inputs['config'].red_components,
+        gwb_components=inputs['config'].gwb_components)
+    return psrs, noise_params, emp_dist
 
-if inputs["config"].mod_sel:
-    pta[1] = pta[0]
+
+def initialize_pta(psrs: list[Pulsar], inputs: dict[str, Any], noise_params : dict | None ) -> dict[int, PTA]:
+    """Initialize the PTA with the user input
+
+    Parameters
+    ----------
+    psrs : list[Pulsar]
+        list of pulsar objects
+    inputs : dict[str, Any]
+        User specified modules
+    noise_params : dict, optional
+        User specified noise params
+
+    Returns
+    -------
+    dict[int, PTA]
+        Dictionary of [enterprise.signals.signal_base.PTA][] objects configured with user inputs
+
+    """
+    pta = {}
 
     pta[0] = signal_builder.builder(
-        psrs=psrs, 
-        model=None, 
+        psrs=psrs,
+        model=inputs['model'],
         noisedict=noise_params,
         pta_dataset=inputs['config'].pta_data,
         bhb_th_prior=inputs['config'].bhb_th_prior,
@@ -99,60 +126,166 @@ if inputs["config"].mod_sel:
         A_bhb_logmin=inputs['config'].A_bhb_logmin,
         A_bhb_logmax=inputs['config'].A_bhb_logmax,
         corr=inputs['config'].corr,
-        red_components=inputs["config"].red_components, 
+        red_components=inputs["config"].red_components,
         gwb_components=inputs["config"].gwb_components)
 
+    if inputs["config"].mod_sel:
+        pta[1] = pta[0]
 
-print('--- Done initializing PTA. ---\n\n')
+        pta[0] = signal_builder.builder(
+            psrs=psrs,
+            model=None,
+            noisedict=noise_params,
+            pta_dataset=inputs['config'].pta_data,
+            bhb_th_prior=inputs['config'].bhb_th_prior,
+            gamma_bhb=inputs['config'].gamma_bhb,
+            A_bhb_logmin=inputs['config'].A_bhb_logmin,
+            A_bhb_logmax=inputs['config'].A_bhb_logmax,
+            corr=inputs['config'].corr,
+            red_components=inputs["config"].red_components,
+            gwb_components=inputs["config"].gwb_components)
+    return pta
 
-###############################################################################
-# define sampler and sample
-###############################################################################
 
-out_dir = os.path.join(
-    inputs["config"].out_dir, inputs["model"].name, f'chain_{input_options["n"]}')
+def setup_sampler(
+        inputs: dict[str, ModuleType],
+        input_options: dict[str, Any],
+        pta: dict[int, PTA],
+        emp_dist: array_like | None,
+) -> tuple[PTSampler, NDArray]:
+    """Setup the PTMCMC sampler
 
-super_model = hypermodel.HyperModel(pta)
+    Parameters
+    ----------
+    inputs : dict[str, ModuleType]
+        Dictionary of loaded user-supplied modules.
+    input_options : dict[str, Any]
+        Dictionary of user-supplied input options.
+    pta : dict[int, PTA]
+        Dictionary of [enterprise.signals.signal_base.PTA][] objects configured with user inputs
+    emp_dist : array_like, optional
+        The empirical distribution to use for sampling
 
-groups = signal_builder.unique_sampling_groups(super_model)
+    Returns
+    -------
+    sampler : PTMCMCSampler.PTSampler
+        Configured [PTMCMCSampler.PTSampler][]
+    x0 : NDArray
+        Initial sample.
 
-if inputs["model"].group:
-    idx_params = [super_model.param_names.index(pp) for pp in inputs["model"].group]
-    [groups.append(idx_params) for _ in range(5)]
+    """
+    out_dir = os.path.join(
+        inputs["config"].out_dir, inputs["model"].name, f'chain_{input_options["n"]}')
 
-# add nmodel index to group structure
-groups.extend([[len(super_model.param_names)-1]])
+    super_model = hypermodel.HyperModel(pta)
 
-sampler = super_model.setup_sampler(
-    resume=inputs["config"].resume,
-    outdir=out_dir,
-    sample_nmodel=inputs["config"].mod_sel,
-    groups=groups,
-    empirical_distr=emp_dist)
+    groups = signal_builder.unique_sampling_groups(super_model)
 
-x0 = super_model.initial_sample()
+    if inputs["model"].group:
+        idx_params = [super_model.param_names.index(pp) for pp in inputs["model"].group]
+        [groups.append(idx_params) for _ in range(5)] # type: ignore
 
-super_model.get_lnlikelihood(x0) # Cache now to make timing more accurate
+    # add nmodel index to group structure
+    groups.extend([[len(super_model.param_names)-1]])
 
-print("Setup times (including first sample) {:.2f} seconds real, {:.2f} seconds CPU\n".format(
-    time.perf_counter()-start_real, time.process_time()-start_cpu));
-start_cpu = time.process_time()
-start_real = time.perf_counter()
+    sampler = super_model.setup_sampler(
+        resume=inputs["config"].resume,
+        outdir=out_dir,
+        sample_nmodel=inputs["config"].mod_sel,
+        groups=groups,
+        empirical_distr=emp_dist)
 
-N_samples = inputs["config"].N_samples
+    x0 = super_model.initial_sample()
 
-print(f'--- Starting to sample {N_samples} samples... ---\n')
+    super_model.get_lnlikelihood(x0) # Cache now to make timing more accurate
 
-sampler.sample(
-    x0, 
-    N_samples,
-    SCAMweight=inputs['config'].scam_weight,
-    AMweight=inputs['config'].am_weight,
-    DEweight=inputs['config'].de_weight)
+    return sampler, x0
 
-print('--- Done sampling. ---')
 
-real_time = time.perf_counter()-start_real
-cpu_time = time.process_time()-start_cpu
-print("Sampling times {:.2f} seconds real =  {:.4f} s/sample, {:.2f} seconds CPU =  {:.4f} s/sample\n".format(
-    real_time, real_time/N_samples, cpu_time, cpu_time/N_samples))
+def do_sample(inputs: dict[str, Any], sampler: PTSampler, x0: NDArray) -> None:
+    """Run the configured sampler.
+
+    Parameters
+    ----------
+    inputs : dict[str, Any]
+        The user specified modules.
+    sampler : PTMCMCSampler.PTSampler
+        The configured [PTMCMCSampler.PTSampler][].
+
+    x0 : NDArray
+        The inital sample.
+
+    """
+    N_samples = inputs["config"].N_samples
+
+    print(f'--- Starting to sample {N_samples} samples... ---\n')
+
+    sampler.sample(
+        x0,
+        N_samples,
+        SCAMweight=inputs['config'].scam_weight,
+        AMweight=inputs['config'].am_weight,
+        DEweight=inputs['config'].de_weight)
+
+    print('--- Done sampling. ---')
+
+
+def main():
+    """Read user inputs, set up sampler and models, and run sampler."""
+    print('\n--- Starting to run ---\n')
+    print("\tNode", platform.node(), cpu_model(),"\n");
+
+    start_cpu = time.process_time()
+    start_real = time.perf_counter()
+
+    ###############################################################################
+    # load inputs
+    ###############################################################################
+
+    # parse command line inputs
+    inputs, input_options = get_user_args()
+
+    ###############################################################################
+    # load pulsars and noise parameters
+    ###############################################################################
+    print('--- Loading Pulsars and noise data ... ---\n')
+
+    # import pta data
+    psrs, noise_params, emp_dist = get_user_pta_data(inputs)
+
+    print('--- Done loading Pulsars and noise data. ---\n')
+
+    ###############################################################################
+    # define models and initialize PTA
+    ###############################################################################
+    print('--- Initializing PTA ... ---\n')
+
+    pta = initialize_pta(psrs, inputs, noise_params)
+
+    print('--- Done initializing PTA. ---\n\n')
+
+    ###############################################################################
+    # define sampler and sample
+    ###############################################################################
+
+    sampler, x0 = setup_sampler(inputs, input_options, pta, emp_dist) # Cache now to make timing more accurate
+
+    print("Setup times (including first sample) {:.2f} seconds real, {:.2f} seconds CPU\n".format(
+        time.perf_counter()-start_real, time.process_time()-start_cpu));
+
+    start_cpu = time.process_time()
+    start_real = time.perf_counter()
+
+    do_sample(inputs, sampler, x0)
+
+
+    real_time = time.perf_counter()-start_real
+    cpu_time = time.process_time()-start_cpu
+
+    N_samples = inputs["config"].N_samples
+    print("Sampling times {:.2f} seconds real =  {:.4f} s/sample, {:.2f} seconds CPU =  {:.4f} s/sample\n".format(
+        real_time, real_time/N_samples, cpu_time, cpu_time/N_samples))
+
+
+if __name__ == "__main__":
+    main()
